002ab2328af98669bc72fef8df8b975940a06e92
xiaozuhui <xiaozuhui@localhost.localdomain>
2021-02-08 13:58:55 +0800

    123

---
+++ data-structure/LinkedList/CMakeFiles/LinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkedList.dir/linked_list.c.o"
+  "CMakeFiles/LinkedList.dir/main.c.o"
+  "LinkedList"
+  "LinkedList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

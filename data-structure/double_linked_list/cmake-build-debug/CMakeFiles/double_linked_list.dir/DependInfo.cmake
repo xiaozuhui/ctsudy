e95d0502ac8e7aedfcda401ada5dbd1e7c6d3adc
xiaozuhui <xiaozuhui@localhost.localdomain>
2021-02-08 21:27:59 +0800

    local

--- data-structure/double_linked_list/cmake-build-debug/CMakeFiles/double_linked_list.dir/DependInfo.cmake
+++ data-structure/double_linked_list/cmake-build-debug/CMakeFiles/double_linked_list.dir/DependInfo.cmake
@@ -4,8 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/mnt/d/codes/cstudy/data-structure/double_linked_list/double_linked_list.c" "/mnt/d/codes/cstudy/data-structure/double_linked_list/cmake-build-debug/CMakeFiles/double_linked_list.dir/double_linked_list.c.o"
-  "/mnt/d/codes/cstudy/data-structure/double_linked_list/main.c" "/mnt/d/codes/cstudy/data-structure/double_linked_list/cmake-build-debug/CMakeFiles/double_linked_list.dir/main.c.o"
+  "/home/xiaozuhui/Codes/cstudy/data-structure/double_linked_list/double_linked_list.c" "/home/xiaozuhui/Codes/cstudy/data-structure/double_linked_list/cmake-build-debug/CMakeFiles/double_linked_list.dir/double_linked_list.c.o"
+  "/home/xiaozuhui/Codes/cstudy/data-structure/double_linked_list/main.c" "/home/xiaozuhui/Codes/cstudy/data-structure/double_linked_list/cmake-build-debug/CMakeFiles/double_linked_list.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 

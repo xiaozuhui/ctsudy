9286568d2434d0a35e06b55f1b9ef3be1d38e153
许子恒 <xuzh@ersoft.cn>
2021-10-11 18:11:14 +0800

    123

--- data-structure/c_stack/rpn.c
+++ data-structure/c_stack/rpn.c
@@ -17,6 +17,15 @@ void generate_rpn(rpn *r, char *exp) {
         strcat(r->finishing_expression, exp_list[i]);
     }
 
+    /**
+     * ( > \*\/\/ > +\/- > )
+     * 1、在括号内，低或平，弹数
+     * 2、在括号内，高，押入
+     * 3、遇到成对括号，整个括号的符号弹出，再弹数
+     * 4、在括号外，低或平，先弹数，再弹符号，再押入符号
+     * 5、遇到结束，先押入所有剩余的数字或符号，然后先弹出所有的数字，再弹出所有的符号
+     */
+
     stack numbers;
     stack operator;
     int length = sizeof(exp_list) / sizeof(*exp_list);
@@ -29,11 +38,65 @@ void generate_rpn(rpn *r, char *exp) {
                 push(&operator, tmp);
             }
             char *opt = peek(&operator, 0);
-            
+            switch (opt[0]) {
+                case '+':
+                case '-':
+                    if (tmp[0] == '+' || tmp[0] == '-') {
+
+                    } else if (tmp[0] == '*' || tmp[0] == '/') {
+
+                    } else if (tmp[0] == '(') {
+
+                    } else if (tmp[0] == ')') {
+
+                    } else {
+
+                    }
+                case '*':
+                case '/':
+                    if (tmp[0] == '+' || tmp[0] == '-') {
+
+                    } else if (tmp[0] == '*' || tmp[0] == '/') {
+
+                    } else if (tmp[0] == '(') {
+
+                    } else if (tmp[0] == ')') {
+
+                    } else {
+
+                    }
+                case '(':
+                    if (tmp[0] == '+' || tmp[0] == '-') {
+
+                    } else if (tmp[0] == '*' || tmp[0] == '/') {
+
+                    } else if (tmp[0] == '(') {
+
+                    } else if (tmp[0] == ')') {
+
+                    } else {
+
+                    }
+                case ')':
+                    if (tmp[0] == '+' || tmp[0] == '-') {
+
+                    } else if (tmp[0] == '*' || tmp[0] == '/') {
+
+                    } else if (tmp[0] == '(') {
+
+                    } else if (tmp[0] == ')') {
+
+                    } else {
+
+                    }
+                default:
+                    continue;
+            }
         }
     }
 
 }
+
 
 void print_rpn(const rpn *r) {
     printf("原公式：%s\n", r->origin_expression);
